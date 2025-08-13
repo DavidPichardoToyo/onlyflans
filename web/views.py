6b875d5381348b3cabdcd9ee4b1621ade7cd1f24
DavidPichardoToyo <dpichardot31@gmail.com>
2025-08-13 17:41:10 +0000

    Hito 2

--- web/views.py
+++ web/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def home(request):
     return render(request, "web/home.html", {})
+
+
+@login_required
+def home_premium(request):
+    return render(request, "web/home_premium.html", {})
